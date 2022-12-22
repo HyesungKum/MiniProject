e8102b185dace5164dc60a698bbeb0d9a22b55ca
ChrisP <jspark4151@gmail.com>
2022-12-23 00:34:55 +0900

    Edited monster & monsterpooling scrips by Jisoo Park

--- Assets/Scripts/Monster_JSP/Monster.cs
+++ Assets/Scripts/Monster_JSP/Monster.cs
@@ -8,32 +8,29 @@ public class Monster : MonoBehaviour
 {
 
     // 몬스터 타입 
+    [Header("Monster Info")]
     [SerializeField] MonsterType mobType;
+    [SerializeField] int MonsterHP;
+    [SerializeField] int monsterSpeed;
+
 
     public MonsterType SetMonsterType { set { mobType = value; } }
 
     public enum MonsterType
     {
         temp1,
-        temp2
+        temp2 = 8
     }
 
-    float monsterSpeed;
-
+    // player 위치확인을 위해
     Playable player = null;
-
-    string myName;
-    Vector3 myDir;
-
 
 
     void Awake()
     {
         // find player 
         player = GameObject.FindObjectOfType<Playable>();
-        myName = this.gameObject.name.Replace("(Clone)", "");
     }
-
 
     private void OnEnable()
     {
@@ -44,27 +41,47 @@ public class Monster : MonoBehaviour
 
     void Update()
     {
+        // HP가 0이되면 파괘 
+        if (MonsterHP <= 0)
+        { 
+            destroyMyself();
+            return;
+        }
 
+
+        // 몬스터 타입에 따른 이동 속도 및 방향 설정
         if (mobType == MonsterType.temp1)
         {
             this.transform.LookAt(player.transform);
-            monsterSpeed = 7f;
+            monsterSpeed = 7;
         }
 
         else if (mobType == MonsterType.temp2)
         {
-            monsterSpeed = 20f;
+            monsterSpeed = 20;
         }
 
         transform.Translate(monsterSpeed * Time.deltaTime * Vector3.forward);
     }
 
-
-
+    // 몬스터를 풀에 넣는다
     void destroyMyself()
     {
         MonsterSpawner.Inst.DestroyMonster(this.gameObject);
     }
 
+
+    // monster damage 관련====================================================================
+    private void OnCollisionEnter(Collision collision)
+    {
+        // weapon에 닿았을 경우 몬스터 체력 감소
+        if (collision.collider.tag == "Weapon")
+        {
+            //MonsterHP -= collision.gameObject.GetComponent<Weapon>().damage;
+            MonsterHP--;
+        }
+    }
+
+    // monster damage 관련====================================================================
 
 }
--- Assets/Scripts/Monster_JSP/MonsterSpawner.cs
+++ Assets/Scripts/Monster_JSP/MonsterSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 using static Monster;
@@ -41,6 +42,7 @@ public class MonsterSpawner : MonoBehaviour
     // Dictionary pooling 
     static Dictionary<string, List<GameObject>> pooling = new Dictionary<string, List<GameObject>>();
 
+    int num;
 
     private void Awake()
     {
@@ -54,6 +56,8 @@ public class MonsterSpawner : MonoBehaviour
     // Generate monster
     public void BringObject(string key)
     {
+        float count = Mathf.Pow(num, num) * num;
+
         GameObject newMonster = null;
 
         // circle의 random한 좌표값 생성 + spawn range 
@@ -63,59 +67,73 @@ public class MonsterSpawner : MonoBehaviour
         randomPos += player.transform.position;
         randomPos.y = 0.5f;
 
-
-        // pooling에 해당 key 값이 없다면 새 List 생성
-        if (!pooling.ContainsKey(key))
+        
+        // 몬스터 타입에 따라. 생성되는 몬스터 수
+        while(count > 0)
         {
-
-            // 새 리스트 생성
-            List<GameObject> monster = new List<GameObject>();
-
-            // 해당 key의 새로운 리스트 추가
-            pooling.Add(key, monster);
-        }
-
-
-        // pool이 비어있다면 새로 생성
-        if (pooling[key].Count == 0)
-        {
-            // key 값의 prefab 불러오기
-            GameObject newKeyMonster = Resources.Load($"Prefabs_JSP\\{key}") as GameObject;
-
-            // 생성 & list에 추가
-            newMonster = Instantiate(newKeyMonster);
-            Debug.Log("생성!");
-        }
-
-        else
-        {
-            foreach (GameObject monster in pooling[key])
+            // pooling에 해당 key 값이 없다면 새 List 생성
+            if (!pooling.ContainsKey(key))
             {
-                // 비활성화된 오브젝트가 있다면
-                if (!monster.activeSelf)
+
+                // 새 리스트 생성
+                List<GameObject> monster = new List<GameObject>();
+
+                // 해당 key의 새로운 리스트 추가
+                pooling.Add(key, monster);
+            }
+
+
+            // pool이 비어있다면 새로 생성
+            if (pooling[key].Count == 0)
+            {
+                // key 값의 prefab 불러오기
+                GameObject newKeyMonster = Resources.Load($"Prefabs_JSP\\{key}") as GameObject;
+
+                // 생성 & list에 추가
+                newMonster = Instantiate(newKeyMonster);
+                Debug.Log("생성!");
+            }
+
+            else
+            {
+                foreach (GameObject monster in pooling[key])
                 {
-                    monster.SetActive(true);
-                    newMonster = monster;
-                    pooling[key].Remove(monster);
-                    break;
+                    // 비활성화된 오브젝트가 있다면
+                    if (!monster.activeSelf)
+                    {
+                        monster.SetActive(true);
+                        newMonster = monster;
+                        pooling[key].Remove(monster);
+                        break;
+                    }
                 }
             }
-        }
 
-        newMonster.transform.position = randomPos;
-        newMonster.transform.LookAt(player.transform.position);
-        newMonster.name = newMonster.name.Replace("(Clone)", "");
 
-        // 생성된 몬스터의 타입 정하기
-        switch (newMonster.name)
-        {
-            case "temp_Monster1":
-                newMonster.GetComponent<Monster>().SetMonsterType = MonsterType.temp1;
-                break;
+            newMonster.transform.position = randomPos;
+            newMonster.transform.LookAt(player.transform);
 
-            case "temp_Monster2":
-                newMonster.GetComponent<Monster>().SetMonsterType = MonsterType.temp2;
-                break;
+            // 일단.. 처음 생성되는 오브젝트 중심으로 좌우에 총 8개 생성.. 
+            newMonster.transform.position += newMonster.transform.right * (count - 4f) * 2;
+
+            newMonster.name = newMonster.name.Replace("(Clone)", "");
+
+            // 생성된 몬스터의 타입 정하기
+            switch (newMonster.name)
+            {
+                case "temp_Monster1":
+                    newMonster.GetComponent<Monster>().SetMonsterType = MonsterType.temp1;
+                    break;
+
+                case "temp_Monster2":
+                    newMonster.GetComponent<Monster>().SetMonsterType = MonsterType.temp2;
+                    break;
+            }
+
+            count--;
+
+            Debug.Log("나의 위치! " + newMonster.transform.position);
+
         }
     }
 
@@ -145,21 +163,12 @@ public class MonsterSpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
 
-        int count = 0;
         while (true)
         {
+            // 랜덤한 몬스터 타입이 나올 수 있도록. 
+            num = Random.Range(1, 3);
 
-            if (count <= 4)
-            {
-                BringObject("temp_Monster1");
-            }
-
-            else if (count > 4)
-            {
-                BringObject("temp_Monster2");
-            }
-
-            count++;
+            BringObject($"temp_Monster{num}");
 
             yield return new WaitForSeconds(2f);
         }

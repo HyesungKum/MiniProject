6c2ae0799440cfd1607b2ad3e338f10a22bdb823
HyesungKum <rmagptjd77@gmail.com>
2022-12-22 14:43:13 +0900

    add joystick by KHS

--- Assets/Joystick_Pack/Scripts/Joysticks/FloatingJoystick.cs
+++ Assets/Joystick_Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using KHS_Axis;
+
+namespace KHS_Axis
+{
+    static public class Axis
+    {
+        static public float GetHorizontal;
+        static public float GetVertical;
+    }
+}
 
 public class FloatingJoystick : Joystick
 {
@@ -22,5 +32,11 @@ public class FloatingJoystick : Joystick
     {
         background.gameObject.SetActive(false);
         base.OnPointerUp(eventData);
+    }
+
+    private void Update()
+    {
+        KHS_Axis.Axis.GetHorizontal = Horizontal;
+        KHS_Axis.Axis.GetVertical = Vertical;
     }
 }
--- Assets/Scripts/Playable/Playable.cs
+++ Assets/Scripts/Playable/Playable.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KHS_Axis;
 
 public class Playable : MonoBehaviour
 {
+    //======================debug controll===================================
+#if UNITY_EDITOR
+    [field : SerializeField] bool DebugConsole { get; set; }
+#endif
+    //======================input mode ========================================
+    enum PlayMode
+    {
+        Touch,
+        Slide
+    }
+    [field: SerializeField] PlayMode playMode { get; set; }
+
+    //==========================level data sheet
     [SerializeField] PC_LevelData[] LevelData = null;
+
     //======================current player data scope==========================
     [field: SerializeField] int curLevel { get; set; }
     [field : SerializeField] float curHp { get; set; }
@@ -16,8 +31,8 @@ public class Playable : MonoBehaviour
     [field: SerializeField] float curHeal { get; set; }
     [field: SerializeField] int curDrop { get; set; }
     [field: SerializeField] int curExp { get; set; }
-    //=========================================================================
 
+    //=========================================================================
     [SerializeField] private AbsorbRange AbsorbRange = null;
 
     private Vector3 moveDir = Vector3.forward;
@@ -34,27 +49,32 @@ public class Playable : MonoBehaviour
         ExpControll();
 
         #region debug
-        if (Input.GetKeyDown(KeyCode.U))
+        #if UNITY_EDITOR
+        if (DebugConsole)
         {
-            if (curLevel < LevelData.Length-1)
-                curLevel++;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if(curLevel>1)
-                curLevel--;
-        }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                if (curLevel < LevelData.Length - 1)
+                    curLevel++;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (curLevel > 1)
+                    curLevel--;
+            }
 
-        Debug.Log($" level    : {LevelData[curLevel - 1].level} "          );
-        Debug.Log($" hp       : {curHp}/{LevelData[curLevel - 1].hp}"      );
-        Debug.Log($" atk      : {LevelData[curLevel - 1].atk}"             );
-        Debug.Log($" def      : {LevelData[curLevel - 1].def}"             );
-        Debug.Log($" speed    : {LevelData[curLevel - 1].speed}"           );
-        Debug.Log($" atkSpeed : {LevelData[curLevel - 1].atkSpeed}"        );
-        Debug.Log($" heal     : {LevelData[curLevel - 1].heal}"            );
-        Debug.Log($" drop     : {LevelData[curLevel - 1].drop}"            );
-        Debug.Log($" Exp      : {curExp}/{LevelData[curLevel - 1].maxExp}" );
-        Debug.Log($"================================================="     );
+            Debug.Log($" level    : {LevelData[curLevel - 1].level} ");
+            Debug.Log($" hp       : {curHp}/{LevelData[curLevel - 1].hp}");
+            Debug.Log($" atk      : {LevelData[curLevel - 1].atk}");
+            Debug.Log($" def      : {LevelData[curLevel - 1].def}");
+            Debug.Log($" speed    : {LevelData[curLevel - 1].speed}");
+            Debug.Log($" atkSpeed : {LevelData[curLevel - 1].atkSpeed}");
+            Debug.Log($" heal     : {LevelData[curLevel - 1].heal}");
+            Debug.Log($" drop     : {LevelData[curLevel - 1].drop}");
+            Debug.Log($" Exp      : {curExp}/{LevelData[curLevel - 1].maxExp}");
+            Debug.Log($"=================================================");
+        }
+        #endif
         #endregion
     }
     //========================parameter controll====================================
@@ -86,15 +106,29 @@ public class Playable : MonoBehaviour
         #if PC
         if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            if (playMode == PlayMode.Touch)
             {
-                Debug.Log(hit.collider.name);
-                if (hit.collider.name == "RayBlock") return;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-                moveDir = new Vector3((hit.point.x - this.transform.position.x),0f, (hit.point.z - this.transform.position.z)).normalized;
-                moveSpeed = LevelData[curLevel - 1].speed * this.transform.forward;
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    Debug.Log(hit.collider.name);
+                    if (hit.collider.name == "RayBlock") return;
+
+                    moveDir = new Vector3((hit.point.x - this.transform.position.x), 0f, (hit.point.z - this.transform.position.z)).normalized;
+                    moveSpeed = LevelData[curLevel - 1].speed * this.transform.forward;
+                }
+            }
+            else if (playMode == PlayMode.Slide)
+            {
+                if (KHS_Axis.Axis.GetHorizontal != 0 || KHS_Axis.Axis.GetVertical != 0)
+                {
+                    float x = KHS_Axis.Axis.GetHorizontal;
+                    float z = KHS_Axis.Axis.GetVertical;
+
+                    moveDir = new Vector3(x, 0f, z).normalized;
+                    moveSpeed = LevelData[curLevel - 1].speed * this.transform.forward;
+                }
             }
         }
         #endif
